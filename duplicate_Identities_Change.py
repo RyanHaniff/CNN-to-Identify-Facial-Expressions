a97bc9fc614c4154a04cbf4d4bb133f01f2d70a6
RyanHaniff <haniffryan.hr@gmail.com>
2022-11-30 16:36:03 -0500

    Cropped the image to just the face (might need to normalize to a certain size)

--- duplicate_Identities_Change.py
+++ duplicate_Identities_Change.py
@@ -1,5 +1,6 @@
 import os
 import re
+import cv2
 
 # TODO
 """Crop the image to the edge of the eye location"""
@@ -18,6 +19,31 @@ def main():
             name = re.findall(r'(?<=\/)S[0-9]+', image_path)
 
             print(name[0])
+
+    data_dir = '../COMP_473_Project/CK+'
+    aug_data_dir = '../COMP_473_Project/CK_Augmented'
+
+    # if not os.path.isdir(aug_data_dir):
+    #     os.mkdir(aug_data_dir)
+
+    for image_class in os.listdir(data_dir):
+        # print(image_class)
+        for image in os.listdir(os.path.join(data_dir, image_class)):
+            image_name = image
+            print(image_name)
+            image_path = os.path.join(data_dir, image_class, image)
+
+            image = cv2.imread(image_path)  # cv2.IMREAD_UNCHANGED
+            face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+            faces = face_cascade.detectMultiScale(image, 1.3, 5)
+            global crop_img
+            for (x, y, w, h) in faces:
+                # To draw a rectangle in a face
+                # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                crop_img = image[y:y + h, x:x + w]
+
+            cv2.imwrite(os.path.join(aug_data_dir, image_class, image_name), crop_img)
 
 
 """

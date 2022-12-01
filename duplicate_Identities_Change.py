4233288ea601f533779759aa5db6f3f1a182736e
Ryan Haniff <haniffryan.hr@gmail.com>
2022-11-30 22:02:33 -0500

    CK_Augmented and CK_Augmented_Crop_Image done

--- duplicate_Identities_Change.py
+++ duplicate_Identities_Change.py
@@ -54,11 +54,15 @@ def image_preprocessing():
 
 
 def eye_preprocessiong():
-    data_dir = '../COMP_473_Project/CK+'
+    # The directory we want to do pre processing on:
+    data_dir = '../COMP_473_Project/CK_Augmented'
+    # The directory where we save the new pre processing images:
     aug_data_dir = '../COMP_473_Project/CK_Augmented_ImageCrop'
 
     if not os.path.isdir(aug_data_dir):
         os.mkdir(aug_data_dir)
+
+    constant_to_scale = 45
 
     for image_class in os.listdir(data_dir):
         for image in os.listdir(os.path.join(data_dir, image_class)):
@@ -73,34 +77,34 @@ def eye_preprocessiong():
             face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
             eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
-            eyes = eye_cascade.detectMultiScale(image, scaleFactor=100, minNeighbors=10)
             faces = face_cascade.detectMultiScale(image, 1.3, 5)
+            eyes = eye_cascade.detectMultiScale(image, scaleFactor=100, minNeighbors=10)
 
-            global crop_img
-            x, y, w, h = 0, 0, 0, 0
+            crop_img = image
+            # x, y, w, h = 0, 0, 0, 0
             for (x, y, w, h) in faces:
                 # To draw a rectangle in a face
                 # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
                 # crop_img = image[y:y + h, x:x + w]
                 x, y, w, h = x, y, w, h
+                crop_img = image[y:y + h, x + constant_to_scale:x + w - constant_to_scale]
 
-            ex, ey, ew, eh = 0, 0, 0, 0
-            for (ex, ey, ew, eh) in eyes:
-                # To draw a rectangle in a face
-                # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                # crop_img = image[ey:ey + eh, ex:ex + ew]
-                ex, ey, ew, eh = ex, ey, ew, eh
-
-            crop_img = image[y:y + h, x:x + w]
-
-            # CK_Augmented_ImageCrop_Class_dir = os.path.join(aug_data_dir, image_class)
+            # ex, ey, ew, eh = 0, 0, 0, 0
+            # if eyes:
+            #     for (ex, ey, ew, eh) in eyes:
+            #         # To draw a rectangle in a face
+            #         # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+            #
+            #         ex, ey, ew, eh = ex, ey, ew, eh
+            #         crop_img = image[y:y + h, ex: ex + ew]
+            #         print("the eyes worked with: {}".format(image_path))
+            # else:
+            #     crop_img = image[y:y + h, x:x + w]
 
             if not os.path.isdir(os.path.join(aug_data_dir, image_class)):
                 os.mkdir(os.path.join(aug_data_dir, image_class))
 
-            # cv2.imwrite(os.path.join(aug_data_dir, image_class, image_name), crop_img)
-
-
+            cv2.imwrite(os.path.join(aug_data_dir, image_class, image_name), crop_img)
 
 
 def data_augmentation():
@@ -115,64 +119,66 @@ def data_augmentation():
 
     for image_class in os.listdir(data_dir):
         for image in os.listdir(os.path.join(data_dir, image_class)):
-            
+
             image_name = image
 
             image_path = os.path.join(data_dir, image_class, image)
 
             img = cv2.imread(image_path)  # cv2.IMREAD_UNCHANGED
-            
-            #create 4 new files after rotation
+
+            # create 4 new files after rotation
             result = []
             rot_angle = [-10, -5, 0, 5, 10]
             for a in rot_angle:
                 imgr = imutils.rotate(img, angle=a)
                 result += [imgr]
-            
 
             count = 0
             for j in range(len(result)):
                 img = result[j]
-                i = math.floor(j/5)
+                i = math.floor(j / 5)
                 x = image_name.split(".")
-                k = (j-(i*5))
-                #rotation angle -10
+                k = (j - (i * 5))
+                # rotation angle -10
                 if k == 0:
-                    newName = x[0] + 'r1'+'.png'
-                #rotation angle -5
+                    newName = x[0] + 'r1' + '.png'
+                # rotation angle -5
                 elif k == 1:
-                    newName = x[0] + 'r2'+'.png'
-                #rotation angle 0 : original image
+                    newName = x[0] + 'r2' + '.png'
+                # rotation angle 0 : original image
                 elif k == 2:
-                    newName = x[0] + 'r3'+'.png'
-                #rotation angle 5
+                    newName = x[0] + 'r3' + '.png'
+                # rotation angle 5
                 elif k == 3:
-                    newName = x[0] + 'r4'+'.png'
-                #rotation angle 10
+                    newName = x[0] + 'r4' + '.png'
+                # rotation angle 10
                 elif k == 4:
-                    newName = x[0] + 'r5'+'.png'
-                
-                #check if there is a directory for the emotion category, if not create one
+                    newName = x[0] + 'r5' + '.png'
+
+                # check if there is a directory for the emotion category, if not create one
                 if not os.path.isdir(os.path.join(aug_data_dir, image_class)):
                     os.mkdir(os.path.join(aug_data_dir, image_class))
-                
-                cv2.imwrite(os.path.join(aug_data_dir, image_class , newName), img)
+
+                cv2.imwrite(os.path.join(aug_data_dir, image_class, newName), img)
+
+def get_image_name_re():
+    data_dir = '../COMP_473_Project/CK+'
+    new_dir = '../COMP_473_Project/CK_formatted'
+
+    for image_class in os.listdir(data_dir):
+        print(image_class +"-----------------------")
+        for image in os.listdir(os.path.join(data_dir, image_class)):
+            image_path = os.path.join(data_dir, image_class, image)
+            # print(image_path)
+
+            name = re.findall(r'(?<=\/)S[0-9]+', image_path)
+
+            print(name[0])
 
 def main():
-    # data_dir = '../COMP_473_Project/CK+'
-    # new_dir = '../COMP_473_Project/CK_formatted'
-    #
-    # for image_class in os.listdir(data_dir):
-    #     print(image_class +"-----------------------")
-    #     for image in os.listdir(os.path.join(data_dir, image_class)):
-    #         image_path = os.path.join(data_dir, image_class, image)
-    #         # print(image_path)
-    #
-    #         name = re.findall(r'(?<=\/)S[0-9]+', image_path)
-    #
-    #         print(name[0])
 
     eye_preprocessiong()
+    # data_augmentation()
 
 
 """
